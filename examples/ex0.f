3c9f29ce01a48b92a6aab5d9da47cc52f985c1ee
StefanoMunari <stefanomunari.sm@gmail.com>
2017-03-30 01:01:11 +0200

    create examples/ in project root

---
+++ examples/ex0.f
@@ -0,0 +1,36 @@
+/** SIMPLE F EXAMPLE */
+/* THIS IS A FRACTIONAL NUMBER */
+___ciao11223333;
+
+bool _var;
+fract foo;
+
+true && false
+
+!v
+
+[ 100 | 13 ] <= [ 11 | 13]
+
+true -> false
+
+[ 12 | 13 ] - [12 | 3]
+
+
+[ 12 | 13 ]
+[ 11                                                                  
+
+| 
+
+
+
+
+
+
+14 ]
+
+
+
+while (true) {
+    bool l;
+    l = true;
+}
