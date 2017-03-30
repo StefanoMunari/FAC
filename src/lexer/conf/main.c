07cb5e13c1d23e13a97ae383bc43b78bd7889c2c
herrBez <mirko.bez@studenti.unipd.it>
2017-03-30 11:59:10 +0200

    Modified main.c in order to be more linear

--- src/lexer/conf/main.c
+++ src/lexer/conf/main.c
@@ -2,16 +2,16 @@ int main(int argc, char * argv[]) {
   if (argc > 1) {
     FILE * fp = fopen(argv[1], "r");
     if (fp == NULL) {
-      fprintf(stderr, "File %s: no such file or directory. The stdin will be used\n", argv[1]);
-    } else {
+      fprintf(stderr, "File %s: no such file or directory\n", argv[1]);
+      return EXIT_FAILURE;
+    } else 
       yyin = fp;
-      yylex();
-      int err_code = fclose(fp);
-      if (err_code == EOF) {
-        fprintf(stderr, "Error occured while closing the file %s\n", argv[1]);
-      }
-    }
+    
   }
   yylex();
+  int err_code = fclose(yyin);
+  if(err_code == EOF) {
+	fprintf(stderr, "Error while cloing input file \n");
+  }
   return EXIT_SUCCESS;
 }
