d10306f952d789f2310da8208443a0274671191d
herrBez <mirko.bez@studenti.unipd.it>
2017-03-30 09:38:29 +0200

    Added normalization of fractional number

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,5 +1,9 @@
 #include "lexer.h"
 
+long long gcd(long long u, long long v) {
+	return (v != 0)?gcd(v, u%v):u;
+}
+
 void tokenize_fract(char * yytext, int yyleng){
 	char * c = malloc(sizeof(char) * (yyleng+1));
 	char * buf0 = malloc(sizeof(char) * (yyleng));
@@ -8,8 +12,11 @@ void tokenize_fract(char * yytext, int yyleng){
 	c[yyleng] = '\0';
 	sscanf(c,"[%[^|]|%[^]]]", buf0, buf1);
 	
-	long long numerator = strtoll(buf0, NULL, 0);
-	long long denominator = strtoll(buf1, NULL, 0);
+	long long numerator = strtol(buf0, NULL, 0);
+	long long denominator = strtol(buf1, NULL, 0);
+	long long g = gcd(numerator, denominator);
+	numerator /= g;
+	denominator /= g;
 	printf("<FRACT,%lld|%lld>\n", numerator, denominator); 
 	
 	free(c);
