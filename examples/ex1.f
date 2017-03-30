be28e906bd2b4385e09a656895210c23624fc24a
herrBez <mirko.bez@studenti.unipd.it>
2017-03-30 11:30:46 +0200

    Added new example + removed old flex file

---
+++ examples/ex1.f
@@ -0,0 +1,18 @@
+/*This is a simple F program */
+
+bool condition;
+fract a;
+fract b;
+fract c;
+
+a = [1|2];
+b = [ 2 | 3 ];
+c = -b + a + b;
+if(condition){
+	print(a);
+}
+
+while(condition){
+	print(a);
+	condition = false;
+}
