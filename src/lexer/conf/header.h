65a261c62c36b67da40c7c6a7c06ee5a519ce628
StefanoMunari <stefanomunari.sm@gmail.com>
2017-04-07 14:51:11 +0200

    fix typos + modify header

--- src/lexer/conf/header.h
+++ src/lexer/conf/header.h
@@ -1,4 +1,9 @@
+/* BISON generated */
+#include "parser.tab.h"
+/* local */
 #include "c_src/lexer.h"
 #include "c_src/factoken.h"
+/* libraries */
 #include <stdbool.h>
+#include <string.h>
 uint line_counter= 1;
