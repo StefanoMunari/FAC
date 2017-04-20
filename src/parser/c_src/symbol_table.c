6afd482b8028fa14398cb3c6f912516dae5a77ce
herrBez <mirko.bez@studenti.unipd.it>
2017-04-20 17:50:39 +0200

    Fixed compilation error due to wrong file path in include statement + Renamed table into symbol_table + It is a working program that can declare variable but not assign them

--- src/parser/c_src/symbol_table.c
+++ src/parser/c_src/symbol_table.c
@@ -1,10 +1,13 @@
 #include "symbol_table.h"
 #include <string.h>
+#include <stdio.h>
+
+extern entry * symbol_table;
 
 void installID(char* _id, type_t _type) {
 	entry * e;
 	
-	HASH_FIND_STR(table, _id, e);
+	HASH_FIND_STR(symbol_table, _id, e);
 	if(e != NULL) { /* ID ALREADY INSTALLED -> ERROR */
 		fprintf(stderr, "The id %s is already installed\n", _id);
 		exit(EXIT_FAILURE);
@@ -14,36 +17,36 @@ void installID(char* _id, type_t _type) {
 	e->id = malloc(sizeof(char) * (strlen(_id) + 1)); //strlen does not take into account '\0'
 	strcpy(e->id, _id);
 	e->type = _type;
-	e->val = NULL;
+	e->value = NULL;
 	
-	HASH_ADD_KEYPTR(hh, table, e->id, strlen(e->id), e); 
+	HASH_ADD_KEYPTR(hh, symbol_table, e->id, strlen(e->id), e); 
 }
 
 
 void* lookupID(char* _id, type_t type) {
 	entry * e;
-	HASH_FIND_STR(table, _id, e);
+	HASH_FIND_STR(symbol_table, _id, e);
 	if(e == NULL){
 		fprintf(stderr, "Failed lookup: The variable %s is not yet installed\n", _id);
 		exit(EXIT_FAILURE);
 	}
 	if(e->type != type){
-		fprintf(stderr, "The requested type differs from the table's one. Exiting\n", _id);
+		fprintf(stderr, "The requested type for %s differs from the symbol_table's one. Exiting\n", _id);
 		exit(EXIT_FAILURE);
 	}
-	return e->val;
+	return e->value;
 }
 
 
-void setValue(char * id, type_t type, void * value) {
+void setValue(char * _id, type_t type, void * value) {
 	entry * e;
-	HASH_FIND_STR(table, _id, e);
+	HASH_FIND_STR(symbol_table, _id, e);
 	if(e == NULL){
-		fprintf(stderr, "The variable cannot be assigned because it not yet installed\n", _id);
+		fprintf(stderr, "The variable cannot be assigned because %s is not yet installed\n", _id);
 		exit(EXIT_FAILURE);
 	}
 	if(e->type != type){
-		fprintf(stderr, "The requested type differs from the table's one. Exiting\n", _id);
+		fprintf(stderr, "The requested type for variable %s differs from the symbol_table's one. Exiting\n", _id);
 		exit(EXIT_FAILURE);
 	}
 	e->value = value;
@@ -53,12 +56,12 @@ void setValue(char * id, type_t type, void * value) {
 
 
 void freeTable(){
-	entry * el, *tmp;
-	HASH_ITER(hh, table, el, tmp) {
-		free(el->id);
-		if(e->val != NULL)
-			free(e->val);
-		HASH_DEL(table, el);
-		free(el);
+	entry * e, *tmp;
+	HASH_ITER(hh, symbol_table, e, tmp) {
+		free(e->id);
+		if(e->value != NULL)
+			free(e->value);
+		HASH_DEL(symbol_table, e);
+		free(e);
 	} 
 }
--- src/parser/c_src/symbol_table.h
+++ src/parser/c_src/symbol_table.h
@@ -1,15 +1,19 @@
+#ifndef __SYMBOL_TABLE_H__
+#define __SYMBOL_TABLE_H__
+#include "../../lexer/c_src/factype.h"
 #include "../lib/uthash.h"
 
 typedef struct _entry {
-	const char * id; /* key */
+	char * id; /* key */
 	type_t type; /* type */
 	void * value; /* Concrete value */
 	UT_hash_handle hh; /* makes this structure hashable */
 } entry;
 
-entry * symbol_table = NULL; // declaration of the variable
 
 void installID(char* id, type_t type);
 void* lookupID(char* id, type_t type);
 void setValue(char * id, type_t type, void * value);
 void freeTable();
+
+#endif /*__SYMBOL_TABLE_H__*/
