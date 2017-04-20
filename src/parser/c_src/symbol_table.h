f74b314a36c5b30f0a7242bc224c23a7745d8069
herrBez <mirko.bez@studenti.unipd.it>
2017-04-20 16:33:05 +0200

    Add definition of entries of the symbol table

--- src/parser/c_src/symbol_table.h
+++ src/parser/c_src/symbol_table.h
@@ -1,4 +1,12 @@
 #include "../lib/uthash.h"
 
+typedef struct _entry {
+	char * id; /* key */
+	type_t type; /* type */
+	void * value; /* Concrete value */
+	UT_hash_handle hh; /* makes this structure hashable */
+} entry;
+
 void installID(char* id, type_t type);
 void* lookupID(char* id, type_t type);
+void setValue(char * id, void * value);
