a33dda11b93de5f2a12cf0c577c62684c4d05523
StefanoMunari <stefanomunari.sm@gmail.com>
2017-05-08 19:10:27 +0200

    make comment (header) conformant with other files

--- src/c_src/type_checker.h
+++ src/c_src/type_checker.h
@@ -1,15 +1,18 @@
 /**
- * @file type_checker.h
+ * @file
+ * @brief Performs type checking on an AST structure
+ * @author <mirko.bez@studenti.unipd.it>
+ * @author <stefano.munari.1@studenti.unipd.it>
  */
 #ifndef __TYPE__CHECKER__H__
 #define __TYPE__CHECKER__H__
 #include "ast.h"
 #include "seq_tree.h"
-/**
- * @param root the root of the abstract syntax tree
- * @return true if the type checking is successful false otherwise
- */
-bool type_checking(seq_node * root);
 
+/** Performs type checking on an AST
+ * @param seq_node the root of the AST
+ * @return bool true if the type checking is successful, false otherwise
+ */
+bool type_checking(seq_node *);
 
 #endif /* __TYPE__CHECKER__H__ */
