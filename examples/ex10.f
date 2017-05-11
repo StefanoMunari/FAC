a1bad42f63f4c40026e90c938b0a5d3a3b665197
herrBez <mirko.bez@studenti.unipd.it>
2017-05-11 16:37:26 +0200

    Add test of if

---
+++ examples/ex10.f
@@ -0,0 +1,31 @@
+
+if(true){
+    skip;
+} else {
+    skip;
+}
+
+
+fract f;
+f = [1|3];
+if(false -> false){
+    f = f + f;
+    print(f);
+} else {
+    f = f - f;
+    print(f);
+}
+
+if(false || [1|3] < [2|3]){
+    f = [1|3];
+} else {
+    skip;
+}
+
+if(f < [1|3] && f > [1|3]){
+    f = [1|3];
+    print(f);
+    print(f);
+} else {
+    print(f);
+}
