8bc649559f792f85063e6b29d32e11b6e954f339
StefanoMunari <stefanomunari.sm@gmail.com>
2017-05-12 18:00:04 +0200

    fix names

--- src/c_src/ast_record.h
+++ src/c_src/ast_record.h
@@ -5,8 +5,8 @@
  * @author <mirko.bez@studenti.unipd.it>
  * @author <stefano.munari.1@studenti.unipd.it>
  */
-#ifndef __ast_RECORD__H__
-#define __ast_RECORD__H__
+#ifndef __AST_RECORD__H__
+#define __AST_RECORD__H__
 #include "factype.h"
 #include "factype_ast.h"
 /**
@@ -22,4 +22,4 @@ typedef struct record {
 record * astRecord(ast_category, op_t, void *);
 void freeastRecord(record *);
 
-#endif /* __ast_RECORD__H__ */
+#endif /* __AST_RECORD__H__ */
