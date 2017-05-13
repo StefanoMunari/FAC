89ae9e9f315343b802f8d4e57b2ef918930b1a9c
herrBez <mirko.bez@studenti.unipd.it>
2017-05-13 20:29:32 +0200

    Add information about line in error handling

--- src/c_src/type_checker_ast.c
+++ src/c_src/type_checker_ast.c
@@ -51,11 +51,11 @@ bool type_check_ast_node(ast_node * ast) {
 			break;
 		default:
 		{
-			char* err_message="%s token not recognized by type_checker";
+			char* err_message="%s token not recognized by type_checker on line %d";
 			int size_token= sizeof(node->data->token);
 			int size_message= (int)strlen(err_message);
 			char err_buffer[size_message+size_token+20];
-			sprintf(err_buffer, err_message, tokenString(node->data->token));
+			sprintf(err_buffer, err_message, tokenString(node->data->token), node->data->line);
 			yyerror(err_buffer);
 		}
 	}
