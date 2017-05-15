ea482ad5ff5b096321c4f2eb2a9f071e991f35c2
StefanoMunari <stefanomunari.sm@gmail.com>
2017-05-16 00:30:18 +0200

    complete test_tac

--- src/c_src/test_tac.c
+++ src/c_src/test_tac.c
@@ -1,10 +1,19 @@
 #include "test_tac.h"
+#include "test_tac.h"
+#include "factype.h"
+#include "tac_list.h"
+#include "tac.h"
 
 void test_tac(tac_node ** head){
 	tac_node * iterator=(* head);
+/*
+	printf("FIRST-OP = %d", iterator->value->op);
+	printf("FIRST-NUM = %d", ((fract_t *) iterator->value->arg0->fract)->num);
+	printf("FIRST-DEN = %d", ((fract_t *) iterator->value->arg0->fract)->den);
+*/
 	int i=0;
 	while(iterator){
-		printf("ITERATOR %p\n", iterator);
+		printf("\n%d)ITERATOR %p\n", i, iterator);
 		iterator=iterator->next;
 		++i;
 	}
