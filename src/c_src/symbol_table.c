4ea82a68cef87912042bffa44c19f0d33c21b525
herrBez <mirko.bez@studenti.unipd.it>
2017-05-15 23:56:33 +0200

    cleaned up and refactored the code for the symbol table

--- src/c_src/symbol_table.c
+++ src/c_src/symbol_table.c
@@ -6,45 +6,35 @@
 extern symbol_table_entry * symbol_table;
 extern void yyerror(char *, ...);
 
-static void check_type (type_t type0, type_t type1) {
-	if(type0 != type1)
-		yyerror("The requested type differs from the symbol_table's one. Exiting");
-};
 
-void installID(char* _id, type_t _type) {
+void installID(char* id, type_t type) {
 	symbol_table_entry * e;
 	HASH_FIND_STR(symbol_table, _id, e);
 	if(e != NULL) { /* ID ALREADY INSTALLED -> ERROR */
 		yyerror("The id %s is already installed", _id);
 	}
 	e = (symbol_table_entry*)malloc(sizeof(symbol_table_entry));
-	e->id = malloc(sizeof(char) * (strlen(_id) + 1)); //strlen does not take into account '\0'
-	strcpy(e->id, _id);
-	e->type = _type;
+	e->id = malloc(sizeof(char) * (strlen(id) + 1)); //strlen does not take into account '\0'
+	strcpy(e->id, id);
+	e->type = type;
 	e->value = NULL;
 
 	HASH_ADD_KEYPTR(hh, symbol_table, e->id, strlen(e->id), e);
 }
 
 
-symbol_table_entry * lookupID(char* _id) {
+symbol_table_entry * lookupID(char* id) {
 	symbol_table_entry * e;
-	HASH_FIND_STR(symbol_table, _id, e);
+	HASH_FIND_STR(symbol_table, id, e);
 	if(e == NULL){
 		yyerror("Failed lookup: The variable %s is not yet installed", _id);
 	}
-	/*
-	check_type(e->type, type);
-	if(e -> value == NULL) {
-		yyerror("The variable %s is declared but not initialized. Exiting", _id);
-	}
-	*/
 	return e;
 }
 
-type_t getType(char * _id) {
+type_t getType(char * id) {
 	symbol_table_entry * e;
-	HASH_FIND_STR(symbol_table, _id, e);
+	HASH_FIND_STR(symbol_table, id, e);
 	if(e == NULL){
 		yyerror("Failed lookup: The variable %s is not yet installed", _id);
 	}
@@ -52,23 +42,12 @@ type_t getType(char * _id) {
 }
 
 
-void setValue(char * _id, void * value) {
+void setValue(char * id, void * value) {
 	symbol_table_entry * e;
-	HASH_FIND_STR(symbol_table, _id, e);
+	HASH_FIND_STR(symbol_table, id, e);
 	if(e == NULL){
 		yyerror("The variable cannot be assigned because %s is not yet installed", _id);
 	}
-	/*
-	switch(type){
-		case FRACT_T :
-			e->value = malloc(sizeof(fract_t));
-			memcpy(e->value, value, sizeof(fract_t));
-			break;
-		case BOOL_T :
-			e->value = malloc(sizeof(bool));
-			memcpy(e->value, value, sizeof(bool));
-			break;
-	}*/
 }
 
 void freeTable(){
