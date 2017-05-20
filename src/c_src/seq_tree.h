a26cb74016d589a4cd1e9559ccd86d965928f875
herrBez <mirko.bez@studenti.unipd.it>
2017-05-20 22:28:10 +0200

    Add name of arguments to c_src/seq_tree.h -> they could make it easier the comments Removed prototype of print_tab since it is local to seq_tree

--- src/c_src/seq_tree.h
+++ src/c_src/seq_tree.h
@@ -1,21 +1,22 @@
 /**
- * @author <mirko.bez@studenti.unipd.it>,<stefano.munari.1@studenti.unipd.it>
- * @file .h
+ * @file
  * @brief file defining the syntax tree data structure and its API
+ * 
+ * @author <mirko.bez@studenti.unipd.it>
+ * @author <stefano.munari.1@studenti.unipd.it>
  */
 #ifndef __SEQ_TREE__H__
 #define __SEQ_TREE__H__
 #include "ast.h"
+
 
 typedef struct seq_node {
 	struct seq_node * left;
 	ast_node * right;
 } seq_node;
 
-
-seq_node * newSeqNode(seq_node *, ast_node *);
-void freeSeqNode(seq_node *);
-int printSeqNode(seq_node *);
-int printSeqNodeRec(seq_node * , int , int );
-void printTab(int tab);
+seq_node * newSeqNode(seq_node * left, ast_node * right);
+void freeSeqNode(seq_node * this);
+int printSeqNode(seq_node * this);
+int printSeqNodeRec(seq_node * this, int instruction, int tab);
 #endif /* __SEQ_TREE__H__ */
