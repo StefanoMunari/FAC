80a969700c698a36596f7ef4de83e3d694f6cb56
StefanoMunari <stefanomunari.sm@gmail.com>
2017-05-20 18:06:15 +0200

    fix types for tac

--- src/c_src/tac.c
+++ src/c_src/tac.c
@@ -7,9 +7,9 @@
 static
 stack_t stack;
 static
-tac_list * tac(seq_node *, tac_list **);
+tac_list * tac(seq_node *, tac_list *);
 static
-tac_list * init_tac(tac_list **);
+tac_list * init_tac(tac_list *);
 static
 void free_tac_value(tac_value * value);
 static
@@ -18,7 +18,7 @@ void free_tac_entry(tac_entry * entry);
 
 tac_list * generate_tac(seq_node * input){
 	tac_list * output=malloc(sizeof(tac_list));
-	output=tac(input, &output);
+	output=tac(input, output);
 	printf("output->first %p\n", output->first);
 	printf("output->last %p\n", output->last);
 	return output;
@@ -27,22 +27,22 @@ tac_list * generate_tac(seq_node * input){
 /*
 	PRE= tlist is pointing to a valid record on the heap
 */
-tac_list * tac(seq_node * node, tac_list ** tlist){
+tac_list * tac(seq_node * node, tac_list * tlist){
 	if(!node->left)
 		return init_tac(tlist);
 	else
-		(*tlist)=tac(node->left, tlist);
+		tlist=tac(node->left, tlist);
 	if(node->right)
-		return tac_ast_node(node->right, (*tlist), &stack);
-	return (*tlist);
+		return tac_ast_node(node->right, tlist, &stack);
+	return tlist;
 }
 
 static
-tac_list * init_tac(tac_list ** tlist){
+tac_list * init_tac(tac_list * tlist){
 	init(&stack);
-	(*tlist)->first=NULL;
-	(*tlist)->last=NULL;
-	return (*tlist);
+	tlist->first=NULL;
+	tlist->last=NULL;
+	return tlist;
 }
 
 void free_tac(tac_list * list){
