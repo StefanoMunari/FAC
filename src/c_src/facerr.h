b281f22ec6ce25905274dc8a6a411e42fb86681d
herrBez <mirko.bez@studenti.unipd.it>
2017-05-24 21:27:00 +0200

    Add documentation to facerr

--- src/c_src/facerr.h
+++ src/c_src/facerr.h
@@ -1,3 +1,11 @@
+/**
+ * @brief definition of fac_err codes and err_handler function. It deals with the
+ * errors encountered during the lexer phase.
+ * @file
+ * @author mirko.bez@studenti.unipd.it
+ * @author stefano.munari.1@studenti.unipd.it
+ */
+
 #ifndef _FACERR_H_
 #define _FACERR_H_
 #include <stdio.h>
@@ -10,7 +18,7 @@ extern int yyleng;
 extern int yylineno;
 
 /** enumeration type used in error handling */
-typedef enum _err_input {
+typedef enum err_input {
 	FAC_STANDARD_ERROR,
 	FAC_STRING,
 	FAC_LINE,
