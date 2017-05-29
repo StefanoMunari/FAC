6110481d98ac08c2dcf55fd93992f15fe4fa25bb
herrBez <mirko.bez@studenti.unipd.it>
2017-05-29 09:29:03 +0200

    Add comments to tac_ast_node.h

--- src/c_src/tac/tac_ast_node.h
+++ src/c_src/tac/tac_ast_node.h
@@ -1,9 +1,17 @@
+/**
+ * @brief Interface that provides the function to trasform an AST tree into a list
+ * of three address code instructions.
+ * @file
+ * @author mirko.bez@studenti.unipd.it
+ * @author stefano.munari.1@studenti.unipd.t
+ */
+
 #ifndef __TAC_AST_NODE_H__
 #define __TAC_AST_NODE_H__
 #include "../ast/ast.h"
 #include "tac.h"
 #include "tac_list.h"
 
-tac_list* tac_ast_node(ast_node *);
+tac_list* tac_ast_node(ast_node * node);
 tac_list* _tac_append(tac_list * , tac_list *);
 #endif /*__TAC_AST_NODE_H__*/
