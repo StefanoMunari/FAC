eb99b01ebdf1bf09001b6a2c812ce05db9615854
StefanoMunari <stefanomunari.sm@gmail.com>
2017-05-31 02:07:33 +0200

    generated main.c from tac_printer_c see #2

--- src/c_src/tac/printer/tac_printer_c.c
+++ src/c_src/tac/printer/tac_printer_c.c
@@ -8,43 +8,47 @@ void print_tac(tac_list *, char *);
 const struct _tprinter_vtable C[] = { { print_tac } };
 
 static
-void print_tac_entry(tac_node *);
+void print_tac_entry(FILE *, tac_node *);
 
 static
 char * get_operator(tac_op operator);
 
 static
-char * getValue(tac_value * tvalue, char * num_or_den);
+char * getValue(tac_value *, char *);
 
 static
-char * getBooleanValue(tac_value * tvalue);
+char * getBooleanValue(tac_value *);
 
 char buffer[256];
 
 void print_tac(tac_list * tlist, char * path){
-	printf("=== C: Print the 3AC ===\n");
-	printf("#include \"fvariables.h\"\n");
-	printf("#include<stdio.h>\n");
-	printf("int MCD(int u, int v) {\n");
-	printf("\treturn (v != 0)?MCD(v, u %c v):u;\n}\n", 37);
+	char * filename =
+		malloc(sizeof(char) * (strlen(path) + 1) + sizeof(char) * (strlen("main.c")));
+	sprintf(filename, "%s%s", path, "main.c");
+	printf("%s\n", filename);
+	FILE * c_source = fopen(filename, "a");
+	fprintf(c_source, "#include \"fvariables.h\"\n");
+	fprintf(c_source, "#include <stdio.h>\n");
+	fprintf(c_source, "int MCD(int u, int v) {\n");
+	fprintf(c_source, "\treturn (v != 0)?MCD(v, u %c v):u;\n}\n", 37);
 
-	printf("int main(void){\n");
+	fprintf(c_source, "int main(void){\n");
 
 	int i=0;
 	tac_node * iterator = tlist->first;
 	while(iterator != NULL){
-		print_tac_entry(iterator);
+		print_tac_entry(c_source, iterator);
 		++i;
 		iterator = iterator->next;
 	}
-	printf("return 0;\n");
-	printf("}\n");
-	printf("=== C: End the 3AC ===\n");
+	fprintf(c_source, "return 0;\n");
+	fprintf(c_source, "}\n");
+	fclose(c_source);
 }
 
 
 
-void print_tac_entry(tac_node * node){
+void print_tac_entry(FILE * c_source, tac_node * node){
 	tac_entry * entry = node->value;
 	if(entry == NULL)
 		return;
@@ -56,15 +60,15 @@ void print_tac_entry(tac_node * node){
 				case BOOL_T:
 				{
 					char * boolean_value = getBooleanValue(entry->arg1);
-					printf("%s = %s;\n", id, boolean_value);
+					fprintf(c_source, "%s = %s;\n", id, boolean_value);
 					break;
 				}
 				case FRACT_T:
 				{
 					char * fract_val_num = getValue(entry->arg1, "num");
 					char * fract_val_den = getValue(entry->arg1, "den");
-					printf("%snum = %s;\n", id, fract_val_num);
-					printf("%sden = %s;\n", id, fract_val_den);
+					fprintf(c_source, "%snum = %s;\n", id, fract_val_num);
+					fprintf(c_source, "%sden = %s;\n", id, fract_val_den);
 					break;
 				}
 			}
@@ -75,8 +79,8 @@ void print_tac_entry(tac_node * node){
 		{
 			char * numA = getValue(entry->arg0, "num");
 			char * denA = getValue(entry->arg0, "den");
-			printf("t%pnum = %s %s;\n", entry, entry->op==TAC_SUM?"+":"-", numA);
-			printf("t%pden = %s %s;\n", entry, entry->op==TAC_SUM?"+":"-", denA);
+			fprintf(c_source, "t%pnum = %s %s;\n", entry, entry->op==TAC_SUM?"+":"-", numA);
+			fprintf(c_source, "t%pden = %s %s;\n", entry, entry->op==TAC_SUM?"+":"-", denA);
 			break;
 		}
 		case TAC_SUM:
@@ -88,14 +92,14 @@ void print_tac_entry(tac_node * node){
 			char * numB = getValue(entry->arg1, "num");
 			char * denB = getValue(entry->arg1, "den");
 
-			printf("/* SUM */\n");
-			printf("h0 = %s * %s;\n", numA, denB);
-			printf("h1 = %s * %s;\n", numB, denA);
-			printf("h2 = h0 %s h1;\n", entry->op==TAC_SUM?"+":"-");
-			printf("h3 = %s * %s;\n", denA, denB);
-			printf("h4 = MCD(h2, h3);\n");
-			printf("t%pnum = h2 / h5;\n", entry);
-			printf("t%pden = h3 / h5;\n", entry);
+			fprintf(c_source, "/* SUM */\n");
+			fprintf(c_source, "h0 = %s * %s;\n", numA, denB);
+			fprintf(c_source, "h1 = %s * %s;\n", numB, denA);
+			fprintf(c_source, "h2 = h0 %s h1;\n", entry->op==TAC_SUM?"+":"-");
+			fprintf(c_source, "h3 = %s * %s;\n", denA, denB);
+			fprintf(c_source, "h4 = MCD(h2, h3);\n");
+			fprintf(c_source, "t%pnum = h2 / h5;\n", entry);
+			fprintf(c_source, "t%pden = h3 / h5;\n", entry);
 
 			free(denA);
 			free(denB);
@@ -118,13 +122,13 @@ void print_tac_entry(tac_node * node){
 				denB = tmp;
 			}
 
-			printf("/* START %s */\n", entry->op == TAC_MULT?"MULT":"DIV");
-			printf("h0 = %s * %s;\n", numA, numB);
-			printf("h1 = %s * %s;\n", denA, denB);
-			printf("h2 = MCD(h0, h1);\n");
-			printf("t%pnum = h0 / h2;\n", entry);
-			printf("t%pden = h1 / h3;\n", entry);
-			printf("/* END %s */\n", entry->op == TAC_MULT?"MULT":"DIV");
+			fprintf(c_source, "/* START %s */\n", entry->op == TAC_MULT?"MULT":"DIV");
+			fprintf(c_source, "h0 = %s * %s;\n", numA, numB);
+			fprintf(c_source, "h1 = %s * %s;\n", denA, denB);
+			fprintf(c_source, "h2 = MCD(h0, h1);\n");
+			fprintf(c_source, "t%pnum = h0 / h2;\n", entry);
+			fprintf(c_source, "t%pden = h1 / h3;\n", entry);
+			fprintf(c_source, "/* END %s */\n", entry->op == TAC_MULT?"MULT":"DIV");
 			free(numA);
 			free(numB);
 			free(denA);
@@ -136,15 +140,15 @@ void print_tac_entry(tac_node * node){
 		{
 			char * id = entry->arg0->address->id;
 			switch(getType(id)){
-				case BOOL_T: printf("printf(\"%cd\", %s)\n", 37, id); break;
-				case FRACT_T: printf("printf(\"[%%d | %%d]\", %snum, %sden)\n", id, id); break;
+				case BOOL_T: fprintf(c_source, "printf(c_source, \"%cd\", %s)\n", 37, id); break;
+				case FRACT_T: fprintf(c_source, "printf(c_source, \"[%%d | %%d]\", %snum, %sden)\n", id, id); break;
 			}
 			break;
 		}
 		case TAC_NOT:
 		{
 			char * boolean_value = getBooleanValue(entry->arg0);
-			printf("t%p = !%s;\n", entry, boolean_value);
+			fprintf(c_source, "t%p = !%s;\n", entry, boolean_value);
 			free(boolean_value);
 			break;
 		}
@@ -153,7 +157,7 @@ void print_tac_entry(tac_node * node){
 		{
 			char * bool1 = getBooleanValue(entry->arg0);
 			char * bool2 = getBooleanValue(entry->arg1);
-			printf("t%p = %s %s %s;\n", entry, bool1,  get_operator(entry->op), bool2);
+			fprintf(c_source, "t%p = %s %s %s;\n", entry, bool1,  get_operator(entry->op), bool2);
 			break;
 		}
 		case TAC_XOR:
@@ -161,7 +165,7 @@ void print_tac_entry(tac_node * node){
 		{
 			char * bool1 = getBooleanValue(entry->arg0);
 			char * bool2 = getBooleanValue(entry->arg1);
-			printf("t%p = %s %s %s; \n", entry, bool1, get_operator(entry->op), bool2);
+			fprintf(c_source, "t%p = %s %s %s; \n", entry, bool1, get_operator(entry->op), bool2);
 			break;
 		}
 		case TAC_EQ:
@@ -172,9 +176,9 @@ void print_tac_entry(tac_node * node){
 			char * denA = getValue(entry->arg0, "den");
 			char * numB = getValue(entry->arg1, "num");
 			char * denB = getValue(entry->arg1, "den");
-			printf("h0 = %s %s %s;\n", numA, get_operator(entry->op), numB);
-			printf("h1 = %s %s %s;\n", denA, get_operator(entry->op), denB);
-			printf("t%p = h0 && h1;\n", entry);
+			fprintf(c_source, "h0 = %s %s %s;\n", numA, get_operator(entry->op), numB);
+			fprintf(c_source, "h1 = %s %s %s;\n", denA, get_operator(entry->op), denB);
+			fprintf(c_source, "t%p = h0 && h1;\n", entry);
 			break;
 		}
 		case TAC_LT:
@@ -186,27 +190,27 @@ void print_tac_entry(tac_node * node){
 			char * denA = getValue(entry->arg0, "den");
 			char * numB = getValue(entry->arg1, "num");
 			char * denB = getValue(entry->arg1, "den");
-			printf("h0 = %s * %s;\n", numA, denB);
-			printf("h1 = %s * %s;\n", denA, numB);
-			printf("t%p = h0 %s h1;\n", entry, get_operator(entry->op));
+			fprintf(c_source, "h0 = %s * %s;\n", numA, denB);
+			fprintf(c_source, "h1 = %s * %s;\n", denA, numB);
+			fprintf(c_source, "t%p = h0 %s h1;\n", entry, get_operator(entry->op));
 			break;
 		}
 		case TAC_LABEL:
 		{
-			printf("L%p:\n", entry);
+			fprintf(c_source, "L%p:\n", entry);
 			break;
 		}
 		case TAC_GOTO:
 		{
 			if(entry->arg1 != NULL){ //Conditional goto
-				printf("if (t%p) goto L%p;\n", entry->arg0->instruction,
+				fprintf(c_source, "if (t%p) goto L%p;\n", entry->arg0->instruction,
 				entry->arg1->instruction);
 			} else {
-				printf("goto L%p;\n", entry->arg0->instruction);
+				fprintf(c_source, "goto L%p;\n", entry->arg0->instruction);
 			}
 			break;
 		}
-		default:printf("Not yet implemented\n");
+		default:fprintf(c_source, "Not yet implemented\n");
 	}
 }
 
