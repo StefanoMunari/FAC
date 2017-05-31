c9f1fc889276b3f9f2f8fadbf2d2c3445db31363
StefanoMunari <stefanomunari.sm@gmail.com>
2017-05-31 14:14:09 +0200

    remove useless function from facmath; correct doc

--- src/c_src/utils/facmath.c
+++ src/c_src/utils/facmath.c
@@ -1,23 +1,15 @@
 #include "facmath.h"
-/** "private" function that computes the gcd of two numbers 
- * @param y
- * @param x
- * @return gcd of x and y
- */
+
 long long gcd(long long x, long long y) {
 	return (y != 0)? gcd(y, x%y) : x;
 }
-/**
- * take as input a fract and returns the normalized fract
- * @param f the fract to normalize
- * @return the normalized fract
- */
+
 fract_t normalizeFract(fract_t f) {
 	long long g = gcd(f.num, f.den);
 	fract_t ret;
 	ret.num = f.num/g;
 	ret.den = f.den/g;
-	/* Normalize sign in order to have minus only on the numerator */
+	/* Normalize sign in order to have negative only on the numerator */
 	if(ret.num < 0 && ret.den < 0){
 		ret.num = -ret.num;
 		ret.den = -ret.den;
@@ -25,27 +17,10 @@ fract_t normalizeFract(fract_t f) {
 	else if(ret.num >= 0 && ret.den < 0) {
 		ret.num = -ret.num;
 		ret.den = -ret.den;
-	} 
+	}
 	return ret;
 }
 
-/**
- * Computes -f. It is used in order to implement difference between two fracts.
- * @param f
- * @return -f
- */
-fract_t minus(fract_t f) {
-	fract_t res;
-	res.num = -f.num;
-	res.den = f.den;
-	return res;
-}
-
-/**
- * Computes the reciprocal of the given fractional number fract1 = [a|b]. 
- * @param fract1 in the form [a|b]
- * @return [b|a]
- */
 fract_t reciprocal(fract_t fract1) {
 	fract_t res;
 	res.num = fract1.den;
@@ -53,12 +28,6 @@ fract_t reciprocal(fract_t fract1) {
 	return res;
 }
 
-/**
- * Returns the multiplication of fract1 and fract2
- * @param fract1
- * @param fract2
- * @return fract1*fract2
- */
 fract_t mult(fract_t fract1, fract_t fract2) {
 	fract_t res;
 	res.num = fract1.num * fract2.num;
@@ -66,12 +35,6 @@ fract_t mult(fract_t fract1, fract_t fract2) {
 	return normalizeFract(res);
 }
 
-/**
- * Returns the sum of fract1 and fract2
- * @param fract1
- * @param fract2
- * @return fract1+fract
- */
 fract_t sum(fract_t fract1, fract_t fract2) {
 	fract_t res;
 	res.num = fract1.num * fract2.den + fract1.den * fract2.num;
@@ -79,12 +42,6 @@ fract_t sum(fract_t fract1, fract_t fract2) {
 	return normalizeFract(res);
 }
 
-/**
- * Computes the lcm of two integer numbers
- * @param a
- * @param b
- * @return lcm(a,b)
- */
 int lcm(int a, int b) {
 	return a*b/gcd(a,b);
 }
