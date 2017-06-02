ab669f2e337480b2683b5e8dcf98d8fe58672169
herrBez <mirko.bez@studenti.unipd.it>
2017-06-02 17:25:15 +0200

    Issue #22 Remove possible memory leaks causes + Removed unused assignment since the variable is then freed

--- src/c_src/tac/tac_list.c
+++ src/c_src/tac/tac_list.c
@@ -2,19 +2,23 @@
 
 tac_list * tac_append(tac_list * tlist, tac_list * to_append){
 
-	if(!to_append->first && !to_append->last)
-		return tlist;
-	if(!tlist->last && !tlist->first){
-		/* first node of the TAC list - nothing to connect */
-		tlist->first=to_append->first;
-		tlist->last=to_append->last;
+	if(!to_append->first && !to_append->last){
+		free(to_append);
 		return tlist;
 	}
-	tlist->last->next=to_append->first;
-	to_append->first->prev=tlist->last;
-	tlist->last=to_append->last;
-	to_append->first=tlist->first;
+	if(!tlist->last && !tlist->first){
+		/* first node of the TAC list - nothing to connect */
+		tlist->first = to_append->first;
+		tlist->last = to_append->last;
+		free(to_append);
+		return tlist;
+	}
+	tlist->last->next = to_append->first;
+	to_append->first->prev = tlist->last;
+	tlist->last = to_append->last;
+	
 	free(to_append);
+	
 	return tlist;
 }
 
