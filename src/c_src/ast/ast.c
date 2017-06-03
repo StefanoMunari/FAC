df1824723fe43c164c3ea8512d362997ffbfb66d
herrBez <mirko.bez@studenti.unipd.it>
2017-06-03 14:31:28 +0000

    Use 80 chars convention in ast.c #23 + Fixed ++i

--- src/c_src/ast/ast.c
+++ src/c_src/ast/ast.c
@@ -20,14 +20,14 @@ ast_node * astNode(unsigned int token, int line, op_t op, void * value,
 		node->ast_children = NULL;
 	}
 	else {
-		node->ast_children = (ast_node**)calloc(number_of_ast_children, sizeof(ast_node*));
+		node->ast_children = calloc(number_of_ast_children, sizeof(ast_node*));
 	}
 	
 	if(node->number_of_seq_children == 0){
 		node->seq_children = NULL;
 	}
 	else {
-		node->seq_children = (seq_node**)calloc(number_of_seq_children, sizeof(seq_node*));
+		node->seq_children = calloc(number_of_seq_children, sizeof(seq_node*));
 	}
 	
 	va_list args_iterator;
@@ -85,7 +85,7 @@ int printastNodeRec(ast_node * node, int instruction, int tab){
 	}
 	putchar('\n');
 
-	for(i = 0; i < node->number_of_ast_children; i++){
+	for(i = 0; i < node->number_of_ast_children; ++i){
 		instruction = printastNodeRec(node->ast_children[i], instruction, tab+1);
 	}
 	for(i = 0; i < node->number_of_seq_children; ++i){
