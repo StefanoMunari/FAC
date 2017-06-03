3147096269b6c3861e2941989ee994fc5364c7f5
herrBez <mirko.bez@studenti.unipd.it>
2017-06-03 16:11:59 +0000

    Issue #25 add spaces between assignments in tac_list.c

--- src/c_src/symbol_table/symbol_table.c
+++ src/c_src/symbol_table/symbol_table.c
@@ -14,11 +14,12 @@ void installID(char* id, type_t type) {
 		yyerror("The id %s is already installed", id);
 	}
 	e = (symbol_table_entry*)malloc(sizeof(symbol_table_entry));
-	e->id = malloc(sizeof(char) * (strlen(id) + 1)); //strlen does not take into account '\0'
+	//strlen does not take into account '\0'
+	e->id = malloc(sizeof(char) * (strlen(id) + 1)); 
 	strcpy(e->id, id);
 	e->type = type;
 	e->value = NULL;
-
+	
 	HASH_ADD_KEYPTR(hh, symbol_table, e->id, strlen(e->id), e);
 }
 
@@ -47,7 +48,8 @@ void setValue(char * id, void * value) {
 	symbol_table_entry * e;
 	HASH_FIND_STR(symbol_table, id, e);
 	if(e == NULL){
-		yyerror("The variable cannot be assigned because %s is not yet installed", id);
+		yyerror("The variable cannot be assigned because %s is not yet installed",
+					id);
 	}
 }
 
