af418019654d8cfaa96592827fbcbabb8dfb5fae
StefanoMunari <stefanomunari.sm@gmail.com>
2017-06-06 20:30:01 +0200

    checked until 'while'; remove a useless instruction; format and provide comments for all the needed steps of the algorithm; see #12

--- src/c_src/tac/tac_ast_node.h
+++ src/c_src/tac/tac_ast_node.h
@@ -1,11 +1,10 @@
 /**
- * @brief Interface that provides the function to trasform an AST tree into a
- * list of three address code instructions.
+ * @brief The core TAC generator. It translates an AST into a
+ * list of indirected triples of TAC instructions.
  * @file
  * @author mirko.bez@studenti.unipd.it
  * @author stefano.munari.1@studenti.unipd.t
  */
-
 #ifndef __TAC_AST_NODE_H__
 #define __TAC_AST_NODE_H__
 #include "../ast/ast.h"
