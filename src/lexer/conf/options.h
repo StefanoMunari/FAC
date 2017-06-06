e089bb7d59e0894d82a8bf6fee44e84838b34392
herrBez <mirko.bez@studenti.unipd.it>
2017-06-06 12:16:48 +0200

    Add some comments to to header and optionsh to remove warnings in doxygen

--- src/lexer/conf/options.h
+++ src/lexer/conf/options.h
@@ -1,3 +1,10 @@
+/** 
+ * @brief some flex options
+ * @file
+ * @author mirko.bez@studenti.unipd.it
+ * @author stefano.munari.1@studenti.unipd.it
+ * 
+ */
 %option align
 %option fast
 %option noyywrap
