f28837a3c1e61323a052d0b9c34091a9caf406c5
StefanoMunari <stefanomunari.sm@gmail.com>
2017-06-06 23:11:06 +0200

    format code and comments; add some comments; see #7; see #12;

--- src/c_src/tac/tac_ast_node.c
+++ src/c_src/tac/tac_ast_node.c
@@ -8,20 +8,16 @@ extern void yyerror(const char *, ...);
 
 static
 tac_node * _tac_node();
-
 static
 tac_node * _tac_print(ast_node *);
 static
 tac_value * _tac_leaf(ast_node * node);
-
 static
 tac_node * _tac_label();
-
 static
 tac_node * _tac_goto_unconditioned(tac_node * destination);
 static
 tac_node * _tac_goto_conditioned(tac_entry * condition, tac_node * destination);
-
 /**
 * @brief builds the 3AC list of triples from the last to the first node of the
 * 		given AST by traversing the AST bottom-up
@@ -40,14 +36,15 @@ tac_list * tac_ast_node(ast_node * node){
 		/* Internal nodes */
 		case AST_AOP1:
 		case AST_BOP1:
-		{
+		{/* Unary Operators */
+			/* create a new node 3AC node */
 			tac_node* tnode=_tac_node();
 			tnode->value->op = node->data->op;
 			tac_value * left = _tac_leaf(node->ast_children[0]);
 			/* left is a leaf */
 			if(left != NULL){
 				tnode->value->arg0 = left;
-			} else {/* left is a tree */
+			} else {/* left is a tree with height > 0 */
 				tac_list * left_list = tac_ast_node(node->ast_children[0]);
 				tnode->value->arg0= calloc(1, sizeof(tac_value));
 				tnode->value->arg0->instruction= left_list->last->value;
@@ -61,7 +58,7 @@ tac_list * tac_ast_node(ast_node * node){
 		case AST_BOP2:
 		case AST_BOP_RELOP:
 		case AST_RELOP:
-		{
+		{/* Binary Operators */
 			/* create a new node 3AC node */
 			tac_node* tnode=_tac_node();
 			tnode->value->op = node->data->op;
@@ -70,7 +67,7 @@ tac_list * tac_ast_node(ast_node * node){
 			/* left is a leaf */
 			if(left != NULL){
 				tnode->value->arg0 = left;
-			} else {/* left is a tree */
+			} else {/* left is a tree with height > 0 */
 				tac_list* left = tac_ast_node(node->ast_children[0]);
 				tnode->value->arg0= calloc(1, sizeof(tac_value));
 				tnode->value->arg0->instruction= left->last->value;
@@ -81,7 +78,7 @@ tac_list * tac_ast_node(ast_node * node){
 			/* right is a leaf */
 			if(right != NULL){
 				tnode->value->arg1 = right;
-			} else {/* right is a tree */
+			} else {/* right is a tree with height > 0 */
 				tac_list* right = tac_ast_node(node->ast_children[1]);
 				tnode->value->arg1= calloc(1, sizeof(tac_value));
 				tnode->value->arg1->instruction= right->last->value;
@@ -91,18 +88,19 @@ tac_list * tac_ast_node(ast_node * node){
 			return tac_connect(tlist, tnode);
 		}
 		case AST_ASSIGNMENT:
-		{
+		{/* Assignment Operator */
+			/* create a new node 3AC node */
 			tac_node* tnode=_tac_node();
 			tnode->value->op = TAC_ASSIGNMENT;
 			tnode->value->arg0 = calloc(1, sizeof(tac_value));
 			tnode->value->arg0->address = (symbol_table_entry*)
 				lookupID(node->ast_children[0]->data->value);
-			/* Compute the value of the right hand side */
+			/* Compute the the right hand side in 3AC */
 			tac_value * leaf = _tac_leaf(node->ast_children[1]);
 			/* the right hand side is a leaf */
 			if(leaf != NULL){
 				tnode->value->arg1 = leaf;
-			} else {/* the right hand side is a tree */
+			} else {/* the right hand side is a tree with height > 0 */
 				tac_list* right = tac_ast_node(node->ast_children[1]);
 				tnode->value->arg1 = calloc(1, sizeof(tac_value));
 				tnode->value->arg1->instruction =  right->last->value;
@@ -112,51 +110,46 @@ tac_list * tac_ast_node(ast_node * node){
 			return tac_connect(tlist, tnode);
 		}
 		case AST_WHILE:
-		{
-			// Create a label for the bexpr and connect it to the actual tlist
+		{/* While Statement */
+			/* Create a label for bexpr and connect it to the actual tlist */
 			tac_node * start_bexpr = _tac_label();
 			tlist = tac_connect(tlist, start_bexpr);
-
-			// Create a label that points to the end of the body
+			/* Create a label that points to the end of the body of the while */
 			tac_node * end_while_label = _tac_label();
-
-			/* Bexpr */
+			/* Compute the bexpr in 3AC */
 			{
 				tac_value * bexpr = _tac_leaf(node->ast_children[0]);
-
-				if(bexpr != NULL){ //It is a leaf
+				/* bexpr is a leaf */
+				if(bexpr != NULL){
 					tac_node * node = _tac_node();
 					node->value->op = TAC_NOT;
 					node->value->arg0 = bexpr;
 					tlist = tac_connect(tlist, node);
-				} else {
+				} else {/* bexpr is a tree with height > 0 */
 					tac_list * bexpr = tac_ast_node(node->ast_children[0]);
 					tac_node * negateCondition = _tac_node();
 					negateCondition->value->op = TAC_NOT;
 					negateCondition->value->arg0 = calloc(1, sizeof(tac_value));
-					negateCondition->value->arg0->instruction = bexpr->last->value;
+					negateCondition->value->arg0->instruction =
+						bexpr->last->value;
 					tlist = tac_append(tlist, bexpr);
 					tlist = tac_connect(tlist, negateCondition);
 				}
 			}
-
-			//If the negate condition is true goto end of while
+			/* If the negate condition is true goto end of while */
 			tac_node * goto_skip_while_body =
 				_tac_goto_conditioned(tlist->last->value, end_while_label);
 			tlist = tac_connect(tlist, goto_skip_while_body);
-
-			//Generate code for the body of the while and append it to the tlist
+			/* Generate code for the body of the while and
+				append it to the tlist */
 			tac_list * stmt = generate_tac(node->seq_children[0]);
 			tlist = tac_append(tlist, stmt);
-
-			// generate goto node that points to the start_bexpr label
+			/* Generate the goto node which points to the start_bexpr label */
 			tac_node * goto_bexpr_evaluation =
 					_tac_goto_unconditioned(start_bexpr);
 			tlist = tac_connect(tlist, goto_bexpr_evaluation);
-
 			/* Add the label to exit from the while loop */
 			tlist = tac_connect(tlist, end_while_label);
-
 			return tlist;
 		}
 		case AST_IF:
@@ -244,8 +237,8 @@ tac_node* _tac_node(){
 	node->value->op = -1;
 	node->value->arg0 = NULL;
 	node->value->arg1 = NULL;
-	node->prev=NULL;
-	node->next=NULL;
+	node->prev = NULL;
+	node->next = NULL;
 	return node;
 }
 
@@ -280,10 +273,10 @@ tac_value * _tac_leaf(ast_node * node){
 
 static
 tac_node * _tac_print(ast_node * node){
-	tac_node * tnode=_tac_node();
-	tnode->value->op= TAC_PRINT;
-	tnode->value->arg0= calloc(1, sizeof(tac_value));
-	tnode->value->arg0->address=
+	tac_node * tnode =_tac_node();
+	tnode->value->op = TAC_PRINT;
+	tnode->value->arg0 = calloc(1, sizeof(tac_value));
+	tnode->value->arg0->address =
 		(symbol_table_entry *) lookupID(node->ast_children[0]->data->value);
 	return tnode;
 }
@@ -291,28 +284,28 @@ static
 tac_node * _tac_label(){
 	tac_node * label = _tac_node();
 	label->value->op = TAC_LABEL;
-	label->value->arg0=NULL;
-	label->value->arg1=NULL;
-	label->prev=NULL;
-	label->next=NULL;
+	label->value->arg0 = NULL;
+	label->value->arg1 = NULL;
+	label->prev = NULL;
+	label->next = NULL;
 	return label;
 }
 
 static
 tac_node * _tac_goto_unconditioned(tac_node * destination){
-	tac_node * goto_node=_tac_node();
-	goto_node->value->op=TAC_GOTO;
-	goto_node->value->arg0=calloc(1, sizeof(tac_value));
-	goto_node->value->arg0->instruction=destination->value;
+	tac_node * goto_node =_tac_node();
+	goto_node->value->op = TAC_GOTO;
+	goto_node->value->arg0 = calloc(1, sizeof(tac_value));
+	goto_node->value->arg0->instruction = destination->value;
 	return goto_node;
 }
 static
 tac_node * _tac_goto_conditioned(tac_entry * condition, tac_node * destination){
-	tac_node * goto_node=_tac_node();
-	goto_node->value->op=TAC_GOTO;
-	goto_node->value->arg0=calloc(1, sizeof(tac_value));
-	goto_node->value->arg0->instruction=condition;
-	goto_node->value->arg1=calloc(1, sizeof(tac_value));
-	goto_node->value->arg1->instruction=destination->value;
+	tac_node * goto_node = _tac_node();
+	goto_node->value->op = TAC_GOTO;
+	goto_node->value->arg0 = calloc(1, sizeof(tac_value));
+	goto_node->value->arg0->instruction = condition;
+	goto_node->value->arg1 = calloc(1, sizeof(tac_value));
+	goto_node->value->arg1->instruction = destination->value;
 	return goto_node;
 }
