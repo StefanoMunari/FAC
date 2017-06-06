92ba6f984ab2acb6d9a77c2464c05766103aae7a
herrBez <mirko.bez@studenti.unipd.it>
2017-06-06 12:16:48 +0200

    Add some comments to to header and optionsh to remove warnings in doxygen

--- src/lexer/conf/header.h
+++ src/lexer/conf/header.h
@@ -1,3 +1,9 @@
+/**
+ * @file
+ * @author mirko.bez@studenti.unipd.it
+ * @author stefano.munari.1@studenti.unipd.it
+ * @brief header of the flex file
+ */
 /* local */
 #include "lexer/lexer.h"
 /* libraries */
