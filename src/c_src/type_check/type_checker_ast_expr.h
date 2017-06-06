ec5ec02c4b377e64aa830935f6c568f8170798e9
herrBez <mirko.bez@studenti.unipd.it>
2017-06-06 09:06:54 +0200

    Fixed doxygen warnings in type check (Issue #26)

--- src/c_src/type_check/type_checker_ast_expr.h
+++ src/c_src/type_check/type_checker_ast_expr.h
@@ -10,11 +10,11 @@
 #include "../types/factype.h"
 /** Type checking on an expression, which is an ast subtree
  * pre-order traversing of the subtree
- * @param ast_node * - the root node of the ast subtree (expression)
- * @param type_t - the type associated with the root node of the subtree
+ * @param ast the root node of the ast subtree (expression)
+ * @param type  the type associated with the root node of the subtree
  * @return bool true if the type checking is successful, false otherwise
  * @see type_checker_ast.h
  */
-bool type_check_ast_expr(ast_node *, type_t);
+bool type_check_ast_expr(ast_node * ast, type_t type);
 
 #endif /* __TYPE__CHECKER__ast__EXPR__H__ */
