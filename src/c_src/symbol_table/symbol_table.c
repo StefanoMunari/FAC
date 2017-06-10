4ae7be99dbf3287e2cdc81e5fc1cdbe0bf31ff3c
herrBez <mirko.bez@studenti.unipd.it>
2017-06-10 15:14:10 +0000

    Removed value from the symbol table and all its appearance in symboltable.c and in the interface (setValue is simply removed). Indeed it was used only when FAC was an interpreter (check parser branch). Issue #7

--- src/c_src/symbol_table/symbol_table.c
+++ src/c_src/symbol_table/symbol_table.c
@@ -18,7 +18,6 @@ void installID(char* id, type_t type) {
 	e->id = malloc(sizeof(char) * (strlen(id) + 1)); 
 	strcpy(e->id, id);
 	e->type = type;
-	e->value = NULL;
 	
 	HASH_ADD_KEYPTR(hh, symbol_table, e->id, strlen(e->id), e);
 }
@@ -48,25 +47,10 @@ type_t getType(char * id) {
 	return e->type;
 }
 
-
-void setValue(char * id, void * value) {
-	symbol_table_entry * e;
-	HASH_FIND_STR(symbol_table, id, e);
-	if(e == NULL){
-		yyerror(
-			"symbol_table::setValue::\
-			The variable cannot be assigned because %s is not yet installed",
-			id
-		);
-	}
-}
-
 void freeTable(){
 	symbol_table_entry * e, *tmp;
 	HASH_ITER(hh, symbol_table, e, tmp) {
 		free(e->id);
-		if(e->value != NULL)
-			free(e->value);
 		HASH_DEL(symbol_table, e);
 		free(e);
 	}
